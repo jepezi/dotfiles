ad6703de5848400a1e03ebb2e446fabfe54be81f
Jirat Arinrith <jepezi@gmail.com>
2015-07-16 21:18:39 +0700

    sublime 3

--- install-deps.sh
+++ install-deps.sh
@@ -2,4 +2,4 @@ cd
 git clone https://github.com/rupa/z.git
 chmod +x ~/z/z.sh
 
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
